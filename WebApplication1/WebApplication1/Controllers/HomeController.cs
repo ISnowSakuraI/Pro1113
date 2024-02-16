1349ba2de676fb70a4d5695d42c4964c9259dd22
Baebabiiboo <kanyanatooo2003@gmail.com>
2024-02-16 17:14:53 +0700

    up proj

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Entities db = new Entities();
+        public ViewResult Index(string sortImg,string searchString)
+        {
+            ViewBag.ImgSortParm = String.IsNullOrEmpty(sortImg) ? "img_desc" : "";
+
+            var img = from p in db.Imgs
+                select p;
+    
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                img = img.Where(p => p.img_name.Contains(searchString)
+                                    || p.img_category.Contains(searchString));
+            }
+            switch (sortImg)
+            {
+                case "img_desc":
+                    img = img.OrderBy(p => p.img_name);
+                    break;
+                default:
+                    img = img.OrderBy(p => p.img_id);
+                    break;
+            }
+            return View(img);
+        }
+
+        public ActionResult MyGallery()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(db.Imgs.ToList().Where(x => x.UserName == User.Identity.Name));
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Img img = db.Imgs.Find(id);
+            if (img == null)
+            {
+                return HttpNotFound();
+            }
+            return View(img);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "img_id,img_name,img_description,img_category,UserName,img_url,img_date")] Img img)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(img).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(img);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Img img = db.Imgs.Find(id);
+            if (img == null)
+            {
+                return HttpNotFound();
+            }
+            List<Comment> comments = db.Comments.Where(r => r.img_id == id).ToList();
+
+            var viewModel = new CommentViewModel
+            {
+                img = img,
+                comments = comments
+            };   
+            return View(viewModel);
+        }
+        public ActionResult MyDetails(int id)
+        {
+            var dataId = db.Imgs.Single(x => x.img_id == id);
+            return View(dataId);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Img img)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var file = Request.Files[0];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Img"), fileName);
+                        file.SaveAs(path);
+                        img.img_url = fileName;
+                    }
+                }
+                img.UserName = User.Identity.Name;
+                img.img_date = DateTime.Now;
+                db.Imgs.Add(img);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Img img = db.Imgs.Find(id);
+            if (img == null)
+            {
+                return HttpNotFound();
+            }
+            return View(img);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Img img = db.Imgs.Find(id);
+            db.Imgs.Remove(img);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Comment(int img_id, string comment)
+        {
+            var CM = new Comment
+            {
+                img_id = img_id,
+                UserName = User.Identity.Name,
+                comment1 = comment,
+                comment_date = DateTime.Now
+            };
+            db.Comments.Add(CM);
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = img_id });
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public JsonResult GetReportJson()
+        {
+            var data = db.Imgs.ToList();
+            return Json(new { JSONList = data }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ImgsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ImgsController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/Imgs
+        public IQueryable<Img> GetImgs()
+        {
+            Uri host = new Uri(Request.RequestUri.ToString());
+            string url = host.GetLeftPart(UriPartial.Authority);
+
+            var prod = db.Imgs.ToList();
+            foreach(var item in prod)
+            {
+                item.img_url = url + "/Img/" + item.img_url;
+            }
+            return db.Imgs;
+        }
+
+        // GET: api/Imgs/5
+        [ResponseType(typeof(Img))]
+        public IHttpActionResult GetImg(int id)
+        {
+            Uri host = new Uri(Request.RequestUri.ToString());
+            string url = host.GetLeftPart(UriPartial.Authority);
+
+            Img img = db.Imgs.Find(id);
+            if (img == null)
+            {
+                return NotFound();
+            }
+            img.img_url = url + "/Img/" + img.img_url;
+
+
+            return Ok(img);
+        }
+
+        // PUT: api/Imgs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutImg(int id, Img img)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != img.img_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(img).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImgExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Imgs
+        [ResponseType(typeof(Img))]
+        public IHttpActionResult PostImg(Img img)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Imgs.Add(img);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = img.img_id }, img);
+        }
+
+        // DELETE: api/Imgs/5
+        [ResponseType(typeof(Img))]
+        public IHttpActionResult DeleteImg(int id)
+        {
+            Img img = db.Imgs.Find(id);
+            if (img == null)
+            {
+                return NotFound();
+            }
+
+            db.Imgs.Remove(img);
+            db.SaveChanges();
+
+            return Ok(img);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ImgExists(int id)
+        {
+            return db.Imgs.Count(e => e.img_id == id) > 0;
+        }
+    }
+}
